79f683859ee856ce13370fa9f9ca61ad9a45fd91
Eugene Salyvon <eugene@coaliance.com>
2020-09-07 21:43:40 +0300

    initial app commit

---
+++ SimpleBlog/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleBlog.Controllers
+{
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        [Route("api/[controller]/GetAll")]
+        public IEnumerable<string> GetAll()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/GetFirst")]
+        public string GetFirst()
+        {
+            return "I'm first";
+        }
+    }
+}
